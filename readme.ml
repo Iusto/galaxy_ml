d5b9f168cc4f66760d8fc0113955f6a54e3581b5
Iusto <85227042+Iusto@users.noreply.github.com>
2023-12-06 13:52:04 +0900

    Update readme.ml

--- readme.ml
+++ readme.ml
@@ -1,3 +1,5 @@
-https://drive.google.com/drive/folders/19rw_h6OzKe_ktBIpfx0_cclelFW6SpKL
+https://drive.google.com/drive/folders/19rw_h6OzKe_ktBIpfx0_cclelFW6SpKL?usp=drive_link
 
 dataset
+  test - 3000
+  train - 30000
